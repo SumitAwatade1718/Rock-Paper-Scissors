915f4c5d139a515b6955121be94297b238f539dc
Sumit Awatade <awatadesumit1718@gmail.com>
2026-03-03 00:24:02 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let userCount = 0;
+let compCount = 0;
+let drawCount = 0;
+const options = ["rock", "paper", "scissors"];
+function action(choice) {
+  document.getElementById("UserChoice").textContent = "Your Choice: " + choice;
+  // Computer random choice
+  const computerChoice = options[Math.floor(Math.random() * 3)];
+
+  document.getElementById("ComputerChoice").textContent =
+    "Computer Choice: " + computerChoice;
+
+  // Decide winner
+  let result = "";
+
+  if (choice === computerChoice) {
+    result = "It's a Draw 🤝";
+    drawCount++;
+  } else if (
+    (choice === "rock" && computerChoice === "scissors") ||
+    (choice === "paper" && computerChoice === "rock") ||
+    (choice === "scissors" && computerChoice === "paper")
+  ) {
+    result = "You Win 🎉";
+    userCount++;
+  } else {
+    result = "Computer Wins 💻";
+    compCount++;
+  }
+
+  document.getElementById("winner").textContent = "Winner: " + result;
+
+  // update score display
+  document.getElementById("userScore").textContent = "Your wins: " + userCount;
+  document.getElementById("compScore").textContent =
+    "Computer wins: " + compCount;
+  document.getElementById("draws").textContent = "Draws: " + drawCount;
+}
